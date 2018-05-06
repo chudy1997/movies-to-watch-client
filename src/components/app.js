e5da6b965d1c4cee73484da15061c554500fe138
chudy1997 <karolbartyzel@interia.pl>
2018-05-06 17:08:33 +0200

    Check heroku deployment

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Movies to watch</div>
+      <div>Movies to watch<p>Heroku</p></div>
+      
     );
   }
 }
