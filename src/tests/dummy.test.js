c9e407cd4a49e51e9db19dc5f79f6284977c2591
chudy1997 <karolbartyzel@interia.pl>
2018-06-13 21:53:29 +0200

    Remove config

--- src/tests/dummy.test.js
+++ src/tests/dummy.test.js
@@ -5,3 +5,11 @@ function sum(a, b) {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+function subtract(a, b) {
+  return a - b;
+}
+
+test('adds 1 + 2 to equal 4', () => {
+  expect(subtract(1, 2)).toBe(-1);
+})
